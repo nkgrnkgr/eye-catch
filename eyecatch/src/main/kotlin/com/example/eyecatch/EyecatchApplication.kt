d562dc447b757399a024e7f767aee443b9de7c51
Hirokuni Maeta <hirokuni-maeta@cybozu.co.jp>
2020-02-21 22:28:46 +0900

    init

---
+++ eyecatch/src/main/kotlin/com/example/eyecatch/EyecatchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eyecatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EyecatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<EyecatchApplication>(*args)
+}
---
+++ eyecatch/src/test/kotlin/com/example/eyecatch/EyecatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.eyecatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EyecatchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

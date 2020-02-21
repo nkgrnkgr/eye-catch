2ab0d9496d13b65b9dab1d55f98da4f1fe611db2
nkgrnkgr <beefupme@gmail.com>
2020-02-22 00:01:49 +0900

    update

--- src/main/kotlin/com/github/nkgrnkgr/eyecatch/api/CardContentController.kt
+++ src/main/kotlin/com/github/nkgrnkgr/eyecatch/api/CardContentController.kt
@@ -2,7 +2,9 @@ package com.github.nkgrnkgr.eyecatch.api
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping("api/card")
 class CardContentController {
 

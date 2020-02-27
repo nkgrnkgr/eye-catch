af57ce6e1a31c364cc660d14dca2feaf841971cc
nkgrnkgr <beefupme@gmail.com>
2020-02-27 22:27:21 +0900

    update for favicon and fullpath

--- src/main/kotlin/com/github/nkgrnkgr/eyecatch/api/EyeCatchService.kt
+++ src/main/kotlin/com/github/nkgrnkgr/eyecatch/api/EyeCatchService.kt
@@ -1,15 +1,20 @@
 package com.github.nkgrnkgr.eyecatch.api
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import org.springframework.stereotype.Service
+import java.net.URI
 
 @Service
 class EyeCatchService {
 
-    fun createEyeCatchData(url: String): EyeCatch {
-        val doc = Jsoup.connect(url).get()
+    fun createEyeCatchData(requestUrl: String): EyeCatch {
+        val doc = Jsoup.connect(requestUrl).get()
         val metadataTags = doc.select("meta")
-        val eyeCatch = EyeCatch.Builder().title(doc.title()).url(url)
+        val favicon = doc.selectFirst("link[rel='shortcut icon']")
+        val icon = doc.selectFirst("link[rel='icon']")
+
+        val eyeCatch = EyeCatch.Builder().title(doc.title()).url(requestUrl).imageUrl(extractHref(favicon, icon))
 
         metadataTags.forEach { metadata ->
             when (metadata.attr("name")) {
@@ -22,12 +27,31 @@ class EyeCatchService {
 
             when (metadata.attr("property")) {
                 "og:title" -> eyeCatch.title(metadata.attr("content"))
-                "og:image" -> eyeCatch.imageUrl(metadata.attr("content"))
+                "og:image" -> { eyeCatch.imageUrl(convertPathRelativeToAbsolute(metadata.attr("content"), requestUrl)) }
                 "og:description" -> eyeCatch.description(metadata.attr("content"))
-                "og:url" -> eyeCatch.url(metadata.attr("content"))
+                "og:url" -> eyeCatch.url(convertPathRelativeToAbsolute(metadata.attr("content"), requestUrl))
             }
         }
 
         return eyeCatch.build()
     }
 }
+
+fun convertPathRelativeToAbsolute(path: String, requestUrl: String): String {
+
+    val requestUri = URI(requestUrl)
+    val pathUri = URI(path)
+
+    if (!requestUri.isAbsolute) throw Exception("""Request Url '${requestUri}' is not absolute. """)
+    if (pathUri.isAbsolute) return pathUri.toString();
+
+    return """${requestUri.scheme}://${requestUri.host}${pathUri}"""
+}
+
+fun extractHref(vararg elements:Element?): String {
+    val list = listOfNotNull(*elements)
+    if (list.isEmpty()) return ""
+
+    return list.first().attr("href")
+}
+
